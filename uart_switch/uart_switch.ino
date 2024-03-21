e78f274f39ff97f4c6d3f2dfbc17ee92e2b359b3
Rada Berar <ujagaga@gmail.com>
2024-03-21 11:42:36 +0100

    Finished pooling

--- uart_switch/uart_switch.ino
+++ uart_switch/uart_switch.ino
@@ -1,13 +1,13 @@
 #include <SoftwareSerial.h>
 
-#define UART_TX_PIN             (4)
-#define BTN_1_PIN               (0)
-#define BTN_2_PIN               (1)
-#define ENC_PIN_A               (2)
-#define ENC_PIN_B               (3)
+#define UART_TX_PIN          (4)
+#define BTN_1_PIN            (3)
+#define BTN_2_PIN            (1)
+#define ENC_PIN_A            (2)
+#define ENC_PIN_B            (0)
 
-#define UPDATE_TIMEOUT          (2)
-#define MSG_START               (0xA5)
+#define UPDATE_TIMEOUT       (2)
+#define MSG_START            (0xA5)
 // Define movement type
 #define DIR_IDLE             (0)
 #define DIR_FWD              (1)
@@ -18,26 +18,39 @@ SoftwareSerial mySerial(255, UART_TX_PIN); // RX, TX
 uint32_t updateTime = 0;
 uint8_t btn_1_pressed = 0;
 uint8_t btn_2_pressed = 0;
-uint8_t old_value = 0;
+uint8_t old_enc_value = 0;
 
 void button_test(){
   btn_1_pressed = 0;
   btn_2_pressed = 0;
 
-  int b1_state = 0;
-  int b2_state = 0;
+  uint8_t b1_state_old = 0;
+  uint8_t b2_state_old = 0;
+  uint8_t b1_state_new = 0;
+  uint8_t b2_state_new = 0;
+  bool test_done = false;
 
   do{  
-    b1_state = digitalRead(BTN_1_PIN);
-    if(b1_state){
+    b1_state_new = digitalRead(BTN_1_PIN);
+    if(b1_state_new != b1_state_old){
+      b1_state_old = b1_state_new;
+    }else if(LOW == b1_state_new){
       btn_1_pressed = 1;
-    }
+    }    
 
-    b2_state = digitalRead(BTN_2_PIN);
-    if(b2_state){
+    b2_state_new = digitalRead(BTN_2_PIN);
+    if(b2_state_new != b2_state_old){
+      b2_state_old = b2_state_new;
+    }else if(HIGH == b2_state_new){
       btn_2_pressed = 1;
     }
-  }while(b1_state || b2_state);
+
+    test_done = (b1_state_new == b1_state_old)  /* Consistent signal */ 
+              && (HIGH == b1_state_new)         /* Button 1 released */ 
+              && (b2_state_new == b2_state_old) /* Consistent signal */
+              && (LOW == b2_state_new);         /* Button 2 released */
+
+  }while(!test_done);
 }
 
 uint8_t read_enc_pins(){
@@ -50,34 +63,34 @@ uint8_t get_enc_dir(){
   uint8_t val = read_enc_pins();
   uint8_t direction = DIR_IDLE;
   
-  if(old_value != val){
+  if(old_enc_value != val){
     if(val == 0){
-      if(old_value == 2){
+      if(old_enc_value == 2){
         direction = DIR_FWD;
       }else{
         direction = DIR_BACK;
       }
     }else if(val == 1){
-      if(old_value == 0){
+      if(old_enc_value == 0){
         direction = DIR_FWD;
       }else{
         direction = DIR_BACK;
       }
     }else if(val == 2){
-      if(old_value == 3){
+      if(old_enc_value == 3){
         direction = DIR_FWD;
       }else{
         direction = DIR_BACK;
       }
     }else{
-      if(old_value == 1){
+      if(old_enc_value == 1){
         direction = DIR_FWD;
       }else{
         direction = DIR_BACK;
       }
     }  
 
-    old_value = val; 
+    old_enc_value = val; 
   }
 
   return direction;
@@ -86,12 +99,12 @@ uint8_t get_enc_dir(){
 void setup(){
   mySerial.begin(115200);
 
-  pinMode(BTN_1_PIN, INPUT);
+  pinMode(BTN_1_PIN, INPUT_PULLUP);
   pinMode(BTN_2_PIN, INPUT);
   pinMode(ENC_PIN_A, INPUT_PULLUP);
   pinMode(ENC_PIN_B, INPUT_PULLUP);
 
-  old_value = read_enc_pins();
+  old_enc_value = read_enc_pins();
 }
 
 void loop(){
